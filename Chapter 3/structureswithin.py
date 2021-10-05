5d4645a22239af255a7f4a80a23b97a622d8f813
VarjoDesigns <57039872+VarjoDesigns@users.noreply.github.com>
2021-10-05 18:08:35 +0300

    Update structureswithin.py

--- Chapter 3/structureswithin.py
+++ Chapter 3/structureswithin.py
@@ -7,6 +7,6 @@ if name == "John":
     if password == "ABC123":
         print("Both inputs are correct!")
     else:
-        print("The password is incorrect")
+        print("The password is incorrect.")
 else:
     print("The given name is wrong.")
